d155d17fb634387b45de9dbcbc96675c83b0fc1e
wowhy <wowhy@outlook.com>
2016-08-22 21:28:25 +0800

    修改柱状图

--- src/example/barchart2/index.js
+++ src/example/barchart2/index.js
@@ -2,26 +2,27 @@ import * as d3 from 'd3'
 
 let width = 400,
   height = 400,
-  padding = { left: 30, right: 30, top: 20, bottom: 20 },
-  rectWidth = 25,
-  rectPadding = 4
+  padding = { left: 30, right: 30, top: 20, bottom: 30 },
+  rectWidth = 30,
+  rectPadding = 5,
+  bodyHeight = height - padding.top - padding.bottom
 
 let svg = d3.select('body').append('svg')
   .attr('width', width)
   .attr('height', height)
 
 let data = [10, 20, 30, 40, 33, 24, 12, 5],
-  maxData = 50
+  maxData = 45
 
-let scaleX = d3.scaleLinear()
-  .domain([0, maxData])
-  .range([0, width - padding.left - padding.right])
+let scaleX = d3.scaleBand()
+  .domain(d3.range(data.length))
+  .range([0, (rectWidth + rectPadding) * data.length + rectPadding])
 
 let scaleY = d3.scaleLinear()
   .domain([0, maxData])
-  .range([height - padding.top - padding.bottom, 0])
+  .range([bodyHeight, 0])
 
-let axisX = null
+let axisX = d3.axisBottom().scale(scaleX)
 let axisY = d3.axisLeft().scale(scaleY)
 
 let bars = svg.selectAll('g')
@@ -34,7 +35,7 @@ let bars = svg.selectAll('g')
 
 bars.append('rect')
   .attr('width', rectWidth)
-  .attr('height', d => height - padding.top - padding.bottom - scaleY(d))
+  .attr('height', d => bodyHeight - scaleY(d))
   .attr('y', d => scaleY(d))
   .attr('fill', 'steelblue')
 
@@ -46,4 +47,8 @@ bars.append('text')
 
 svg.append('g')
   .call(axisY)
-  .attr('transform', 'translate(' + (padding.left - 5) + ', ' + padding.top + ')')
+  .attr('transform', 'translate(' + (padding.left - rectPadding) + ', ' + padding.top + ')')
+
+svg.append('g')
+  .call(axisX)
+  .attr('transform', 'translate(' + (padding.left - rectPadding) + ', ' + (height - padding.bottom) + ')')
